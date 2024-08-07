70ff578f79558a1df56ef1285b6bc8636b82abb7
bolly-biophysics <107992592+bolly-biophysics@users.noreply.github.com>
2024-08-07 16:40:53 +0800

    Add files via upload

---
+++ analysis/s_b_series.cpp
@@ -0,0 +1,183 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#define N_tot 100
+#define r_cut 3
+using namespace std;
+
+float p1_x[60000][40], p1_y[60000][40], p1_z[60000][40];
+float p2_x[60000][40], p2_y[60000][40], p2_z[60000][40];
+float p3_x[60000][40], p3_y[60000][40], p3_z[60000][40];
+float n1_x[60000][30], n1_y[60000][30], n1_z[60000][30];
+float n2_x[60000][30], n2_y[60000][30], n2_z[60000][30];
+float s_b[60000][30][30];
+float s_b_total[60000];
+
+struct example
+{
+    char name[5];
+    float x, y, z;
+};
+
+example atom_info[60000][200];
+
+int main()
+{
+    ifstream infile;
+    infile.open("side_chain.pdb");
+
+    if (!infile)
+    {
+        cerr << "Open infile failure!" << endl;
+        return -1;
+    }
+
+    // 将P原子坐标格式化存入结构数组
+    int i = 1, j, k, l, m, t, count;
+    float aa;
+    while (!infile.eof())
+    {
+        aa = (i + 0.0) / (N_tot + 0.0); 
+        k = fmod(i, N_tot);
+        if (k == 0) 
+        {
+            k = N_tot;
+            j = floor(aa);
+        }
+        else 
+            j = floor(aa) + 1;
+        infile >> atom_info[j][k].name >> atom_info[j][k].x >> atom_info[j][k].y >> atom_info[j][k].z;
+        i++;
+    }
+    t = j - 1;
+    cout << t << endl;
+    infile.close();
+
+    ofstream outfile_1;
+    outfile_1.open("s_b_series.dat", ios::out | ios::ate);
+
+    // 计算ARG电正中心1随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 1; j <= 30; j = j + 3)
+        {
+            p1_x[i][(j + 2) / 3] = atom_info[i][j].x;
+            p1_y[i][(j + 2) / 3] = atom_info[i][j].y;
+            p1_z[i][(j + 2) / 3] = atom_info[i][j].z;
+        }
+        for (j = 51; j <= 80; j = j + 3)
+        {
+            p1_x[i][(j - 18) / 3] = atom_info[i][j].x;
+            p1_y[i][(j - 18) / 3] = atom_info[i][j].y;
+            p1_z[i][(j - 18) / 3] = atom_info[i][j].z;
+        }
+    }
+
+    // 计算ARG电正中心2随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 2; j <= 30; j = j + 3)
+        {
+            p2_x[i][(j + 1) / 3] = atom_info[i][j].x;
+            p2_y[i][(j + 1) / 3] = atom_info[i][j].y;
+            p2_z[i][(j + 1) / 3] = atom_info[i][j].z;
+        }
+        for (j = 52; j <= 80; j = j + 3)
+        {
+            p2_x[i][(j - 19) / 3] = atom_info[i][j].x;
+            p2_y[i][(j - 19) / 3] = atom_info[i][j].y;
+            p2_z[i][(j - 19) / 3] = atom_info[i][j].z;
+        }
+    }
+
+    // 计算ARG电正中心3随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 3; j <= 30; j = j + 3)
+        {
+            p3_x[i][(j + 0) / 3] = atom_info[i][j].x;
+            p3_y[i][(j + 0) / 3] = atom_info[i][j].y;
+            p3_z[i][(j + 0) / 3] = atom_info[i][j].z;
+        }
+        for (j = 53; j <= 80; j = j + 3)
+        {
+            p3_x[i][(j - 20) / 3] = atom_info[i][j].x;
+            p3_y[i][(j - 20) / 3] = atom_info[i][j].y;
+            p3_z[i][(j - 20) / 3] = atom_info[i][j].z;
+        }
+    }
+    
+    // 计算ASP电负中心1随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 31; j <= 50; j = j + 2)
+        {
+            n1_x[i][(j - 29) / 2] = atom_info[i][j].x;
+            n1_y[i][(j - 29) / 2] = atom_info[i][j].y;
+            n1_z[i][(j - 29) / 2] = atom_info[i][j].z;
+        }
+        for (j = 81; j <= 100; j = j + 2)
+        {
+            n1_x[i][(j - 59) / 2] = atom_info[i][j].x;
+            n1_y[i][(j - 59) / 2] = atom_info[i][j].y;
+            n1_z[i][(j - 59) / 2] = atom_info[i][j].z;
+        }
+    }
+
+    // 计算ASP电负中心2随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 32; j <= 50; j = j + 2)
+        {
+            n2_x[i][(j - 30) / 2] = atom_info[i][j].x;
+            n2_y[i][(j - 30) / 2] = atom_info[i][j].y;
+            n2_z[i][(j - 30) / 2] = atom_info[i][j].z;
+        }
+        for (j = 82; j <= 100; j = j + 2)
+        {
+            n2_x[i][(j - 60) / 2] = atom_info[i][j].x;
+            n2_y[i][(j - 60) / 2] = atom_info[i][j].y;
+            n2_z[i][(j - 60) / 2] = atom_info[i][j].z;
+        }
+    }
+    
+    // 计算正负残基对之间盐桥数目随时间的变化
+    float d[5];
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 1; j <= 20; j++)
+        {
+            for (k = 1; k <= 20; k++)
+            {
+                d[1] = sqrt((n1_x[i][k] - p1_x[i][j]) * (n1_x[i][k] - p1_x[i][j]) + (n1_y[i][k] - p1_y[i][j]) * (n1_y[i][k] - p1_y[i][j]) + (n1_z[i][k] - p1_z[i][j]) * (n1_z[i][k] - p1_z[i][j]));
+                d[2] = sqrt((n1_x[i][k] - p2_x[i][j]) * (n1_x[i][k] - p2_x[i][j]) + (n1_y[i][k] - p2_y[i][j]) * (n1_y[i][k] - p2_y[i][j]) + (n1_z[i][k] - p2_z[i][j]) * (n1_z[i][k] - p2_z[i][j]));
+                d[3] = sqrt((n1_x[i][k] - p3_x[i][j]) * (n1_x[i][k] - p3_x[i][j]) + (n1_y[i][k] - p3_y[i][j]) * (n1_y[i][k] - p3_y[i][j]) + (n1_z[i][k] - p3_z[i][j]) * (n1_z[i][k] - p3_z[i][j]));
+                d[4] = sqrt((n2_x[i][k] - p1_x[i][j]) * (n2_x[i][k] - p1_x[i][j]) + (n2_y[i][k] - p1_y[i][j]) * (n2_y[i][k] - p1_y[i][j]) + (n2_z[i][k] - p1_z[i][j]) * (n2_z[i][k] - p1_z[i][j]));
+                d[5] = sqrt((n2_x[i][k] - p2_x[i][j]) * (n2_x[i][k] - p2_x[i][j]) + (n2_y[i][k] - p2_y[i][j]) * (n2_y[i][k] - p2_y[i][j]) + (n2_z[i][k] - p2_z[i][j]) * (n2_z[i][k] - p2_z[i][j]));
+                d[6] = sqrt((n2_x[i][k] - p3_x[i][j]) * (n2_x[i][k] - p3_x[i][j]) + (n2_y[i][k] - p3_y[i][j]) * (n2_y[i][k] - p3_y[i][j]) + (n2_z[i][k] - p3_z[i][j]) * (n2_z[i][k] - p3_z[i][j]));
+                count = 0;
+                for (l = 1; l <= 6; l++)
+                {
+                    if (d[l] < r_cut)
+                        count++;
+                }
+                s_b[i][j][k] = count;
+            }
+        }
+    }
+
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 1; j <= 20; j++)
+        {
+            for (k = 1; k <= 20; k++)
+            {
+                s_b_total[i] += s_b[i][j][k];
+            }
+        }
+        outfile_1 << i << " " << s_b_total[i] << endl;
+    }
+    outfile_1.close();
+
+    return 0;
+}
---
+++ analysis/stacking_env.cpp
@@ -0,0 +1,246 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#define N_ele_env 80
+#define N_CZ 20
+#define interval 10
+#define stacking_dist 4.2
+#define cut 10
+using namespace std;
+
+float p_center_x[60000][30], p_center_y[60000][30], p_center_z[60000][30];
+float n_center_x[60000][30], n_center_y[60000][30], n_center_z[60000][30];
+float CZ_ARG_env[60000][30], CZ_ASP_env[60000][30];
+float CZ_pair_env[60000][30][30], CZ_pair_dist[60000][30][30];
+float CZ_pair_dist_interval[60000][30][30], CZ_pair_env_interval[60000][30][30];
+float p_dist_env[100][100];
+float CZ_pair_dist_avg[30][30], CZ_pair_env_avg[30][30];
+
+struct example
+{
+    char name[5];
+    float x, y, z;
+};
+
+example CZ_info[60000][30];
+example ele_env_info[60000][100];
+
+int main()
+{
+    ifstream infile;
+    infile.open("CZ.pdb");
+
+    if (!infile)
+    {
+        cerr << "Open infile failure!" << endl;
+        return -1;
+    }
+
+    // 将静电环境坐标格式化存入结构数组
+    int i = 1, j, k, l, m, t;
+    float aa;
+    while (!infile.eof())
+    {
+        aa = (i + 0.0) / (N_CZ + 0.0); 
+        k = fmod(i, N_CZ);
+        if (k == 0) 
+        {
+            k = N_CZ;
+            j = floor(aa);
+        }
+        else 
+            j = floor(aa) + 1;
+        infile >> CZ_info[j][k].name >> CZ_info[j][k].x >> CZ_info[j][k].y >> CZ_info[j][k].z;
+        i++;
+    }
+    t = j - 1;
+    cout << t << endl;
+    infile.close();
+
+    infile.open("../res_env.pdb");
+
+    if (!infile)
+    {
+        cerr << "Open infile failure!" << endl;
+        return -1;
+    }
+
+    // 将CZ原子坐标格式化存入结构数组
+    i = 1;
+    while (!infile.eof())
+    {
+        aa = (i + 0.0) / (N_ele_env + 0.0); 
+        k = fmod(i, N_ele_env);
+        if (k == 0) 
+        {
+            k = N_ele_env;
+            j = floor(aa);
+        }
+        else 
+            j = floor(aa) + 1;
+        infile >> ele_env_info[j][k].name >> ele_env_info[j][k].x >> ele_env_info[j][k].y >> ele_env_info[j][k].z;
+        i++;
+    }
+    t = j - 1;
+    cout << t << endl;
+    infile.close();
+
+    ofstream outfile_1, outfile_2, outfile_3, outfile_4;
+    outfile_1.open("stacking_env.dat", ios::out | ios::ate);
+    outfile_2.open("p_dist_env.dat", ios::out | ios::ate);
+    //outfile_3.open("CZ_pair_dist.dat", ios::out | ios::ate);
+    //outfile_4.open("CZ_pair_env.dat", ios::out | ios::ate);
+
+    // 计算ARG电正中心随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 1; j <= 20; j = j + 2)
+        {
+            p_center_x[i][(j + 1) / 2] = (ele_env_info[i][j].x + ele_env_info[i][j + 1].x) / 2;
+            p_center_y[i][(j + 1) / 2] = (ele_env_info[i][j].y + ele_env_info[i][j + 1].y) / 2;
+            p_center_z[i][(j + 1) / 2] = (ele_env_info[i][j].z + ele_env_info[i][j + 1].z) / 2;
+        }
+        for (j = 41; j <= 60; j = j + 2)
+        {
+            p_center_x[i][(j - 19) / 2] = (ele_env_info[i][j].x + ele_env_info[i][j + 1].x) / 2;
+            p_center_y[i][(j - 19) / 2] = (ele_env_info[i][j].y + ele_env_info[i][j + 1].y) / 2;
+            p_center_z[i][(j - 19) / 2] = (ele_env_info[i][j].z + ele_env_info[i][j + 1].z) / 2;
+        }
+    }
+    
+    // 计算ASP电负中心随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 21; j <= 40; j = j + 2)
+        {
+            n_center_x[i][(j - 19) / 2] = (ele_env_info[i][j].x + ele_env_info[i][j + 1].x) / 2;
+            n_center_y[i][(j - 19) / 2] = (ele_env_info[i][j].y + ele_env_info[i][j + 1].y) / 2;
+            n_center_z[i][(j - 19) / 2] = (ele_env_info[i][j].z + ele_env_info[i][j + 1].z) / 2;
+        }
+        for (j = 61; j <= 80; j = j + 2)
+        {
+            n_center_x[i][(j - 39) / 2] = (ele_env_info[i][j].x + ele_env_info[i][j + 1].x) / 2;
+            n_center_y[i][(j - 39) / 2] = (ele_env_info[i][j].y + ele_env_info[i][j + 1].y) / 2;
+            n_center_z[i][(j - 39) / 2] = (ele_env_info[i][j].z + ele_env_info[i][j + 1].z) / 2;
+        }
+    }
+
+    // 计算CZ周围ARG给予电正环境随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 1; j <= 20; j++)
+        {
+            CZ_ARG_env[i][j] = 0;
+            for (k = 1; k <= 20; k++)
+            {
+                CZ_ARG_env[i][j] += 1 / sqrt((p_center_x[i][k] - CZ_info[i][j].x) * (p_center_x[i][k] - CZ_info[i][j].x) + (p_center_y[i][k] - CZ_info[i][j].y) * (p_center_y[i][k] - CZ_info[i][j].y) + (p_center_z[i][k] - CZ_info[i][j].z) * (p_center_z[i][k] - CZ_info[i][j].z));
+            }
+        }
+    }
+
+    // 计算CZ周围ASP给予电负环境随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 1; j <= 20; j++)
+        {
+            CZ_ASP_env[i][j] = 0;
+            for (k = 1; k <= 20; k++)
+            {
+                CZ_ASP_env[i][j] += -1 / sqrt((n_center_x[i][k] - CZ_info[i][j].x) * (n_center_x[i][k] - CZ_info[i][j].x) + (n_center_y[i][k] - CZ_info[i][j].y) * (n_center_y[i][k] - CZ_info[i][j].y) + (n_center_z[i][k] - CZ_info[i][j].z) * (n_center_z[i][k] - CZ_info[i][j].z));
+            }
+        }
+    }
+
+    // 计算CZ配对之间距离以及总环境随时间的变化
+    for (i = 1; i <= t; i++)
+    {
+        for (j = 1; j <= 20; j++)
+        {
+            for (k = 1; k <= 20; k++)
+            {
+                CZ_pair_dist[i][j][k] = sqrt((CZ_info[i][j].x - CZ_info[i][k].x) * (CZ_info[i][j].x - CZ_info[i][k].x) + (CZ_info[i][j].y - CZ_info[i][k].y) * (CZ_info[i][j].y - CZ_info[i][k].y) + (CZ_info[i][j].z - CZ_info[i][k].z) * (CZ_info[i][j].z - CZ_info[i][k].z));
+                CZ_pair_env[i][j][k] = CZ_ASP_env[i][j] + CZ_ASP_env[i][k];
+            }
+        }
+    }
+
+    // 计算CZ配对之间距离以及总环境随时间的变化（累积）
+    float CZ_pair_dist_tmp, CZ_pair_env_tmp;
+    for (i = 1; i <= t; i = i + interval)
+    {
+        for (j = 1; j <= 20; j++)
+        {
+            for (k = 1; k <= 20; k++)
+            {
+                if (j > k)
+                {
+                    CZ_pair_dist_tmp = 0, CZ_pair_env_tmp = 0;
+                    for (l = 1; l <= interval; l++)
+                    {
+                        CZ_pair_dist_tmp += CZ_pair_dist[i + l - 1][j][k];
+                        CZ_pair_env_tmp += CZ_pair_env[i + l - 1][j][k];
+                    }
+                    CZ_pair_dist_interval[(i + interval - 1) / interval][j][k] = CZ_pair_dist_tmp / interval;
+                    CZ_pair_env_interval[(i + interval - 1) / interval][j][k] = CZ_pair_env_tmp / interval;
+                    outfile_1 << CZ_pair_dist_interval[(i + interval - 1) / interval][j][k] << " " << CZ_pair_env_interval[(i + interval - 1) / interval][j][k] << endl;
+                }
+            }
+        }
+    }
+    outfile_1.close();
+
+    // 计算CZ配对之间距离以及总环境关联的二维分布
+    int count;
+    for (i = 0; i <= 60; i++)
+    {
+        for (j = -5 * cut; j <= -1 * cut; j++)
+        {
+            count = 0;
+            for (k = 1; k <= t / interval - 1; k++)
+            {
+                for (l = 1; l <= 20; l++)
+                {
+                    for (m = 1; m <= 20; m++)
+                    {
+                        if (l > m)
+                        {
+                            if (CZ_pair_dist_interval[k][l][m] >= i && CZ_pair_dist_interval[k][l][m] < i + 1)
+                            {
+                                if (CZ_pair_env_interval[k][l][m] >= (float)j / cut && CZ_pair_env_interval[k][l][m] < (float)(j + 1) / cut)
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            p_dist_env[i][j] = (float)count / (t / interval) / 190;
+            outfile_2 << i << " " << (float)j / cut << " " << p_dist_env[i][j] << endl;
+        }
+        outfile_2 << endl;
+    }
+    outfile_2.close();
+
+    // 计算CZ配对之间距离及总环境的平均值
+/*    for (i = 1; i <= 20; i++)
+    {
+        for (j = 1; j <= 20; j++)
+        {
+            for (k = 1; k <= t / interval - 1; k++)
+            {
+                if (CZ_pair_dist_interval[k][i][j] < stacking_dist)
+                    CZ_pair_dist_avg[i][j]++;
+                CZ_pair_env_avg[i][j] += CZ_pair_env_interval[k][i][j];
+            }
+            CZ_pair_dist_avg[i][j] /= t / interval;
+            CZ_pair_env_avg[i][j] /= t / interval;
+            outfile_3 << i << " " << j << " " << CZ_pair_dist_avg[i][j] << endl;
+            outfile_4 << i << " " << j << " " << CZ_pair_env_avg[i][j] << endl;
+        }
+        outfile_3 << endl;
+        outfile_4 << endl;
+    }
+    outfile_3.close();
+    outfile_4.close();*/
+
+    return 0;
+}
